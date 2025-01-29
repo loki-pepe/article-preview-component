6bee45cea571a9fe91f4e407174416a794feae29
Lovro <lovroperaic@gmail.com>
2025-01-29 16:06:54 +0100

    add click listener to hide share overlay if clicked elsewhere

--- static/script.js
+++ static/script.js
@@ -2,6 +2,12 @@ let shareButton = document.getElementById('share');
 let shareOverlay = document.getElementById('footer-overlay');
 
 shareButton.addEventListener('click', () => toggleShareOverlay());
+document.addEventListener('click', (e) => {
+    if (!shareOverlay.contains(e.target) && !shareButton.contains(e.target) && shareOverlay.classList.contains('active')) {
+        toggleShareOverlay();
+    }
+})
+
 function toggleShareOverlay() {
     shareButton.classList.toggle('selected');
     shareOverlay.classList.toggle('active');
